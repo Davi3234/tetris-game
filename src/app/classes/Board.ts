7863f4532cd8a0ea0b040c2ff9854c66e191e6f5
Davi Gabriel Scottini Adriano <davi.fadriano@gmail.com>
2025-01-09 21:50:07 -0300

    fix: Changing board to have color

--- src/app/classes/Board.ts
+++ src/app/classes/Board.ts
@@ -3,38 +3,46 @@ import { Piece } from "./Piece";
 export class Board {
   width: number;
   height: number;
-  grid: number[][];
+  grid: ({ value: number; color: string })[][];
 
   constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
-    this.grid = Array.from({ length: height }, () => Array(width).fill(0));
+    this.grid = Array.from({ length: height }, () =>
+      Array(width).fill({ value: 0, color: '' })
+    );
   }
 
   clearRow(row: number) {
     this.grid.splice(row, 1);
-    this.grid.unshift(Array(this.width).fill(0));
+    this.grid.unshift(Array(this.width).fill({ value: 0, color: '' }));
   }
 
   isRowFull(row: number): boolean {
-    return this.grid[row].every(cell => cell !== 0);
+    return this.grid[row].every(cell => cell.value !== 0);
   }
 
-  addPiece(piece : Piece) {
+  addPiece(piece: Piece) {
     for (let i = 0; i < piece.shape.length; i++) {
       for (let j = 0; j < piece.shape[i].length; j++) {
         if (piece.shape[i][j] && this.grid[piece.position.y + i] !== undefined) {
-          this.grid[piece.position.y + i][piece.position.x + j] = piece.shape[i][j];
+          this.grid[piece.position.y + i][piece.position.x + j] = {
+            value: piece.shape[i][j],
+            color: piece.color,
+          };
         }
       }
     }
   }
 
-  cleanPiece(piece : Piece){
+  cleanPiece(piece: Piece) {
     for (let i = 0; i < piece.shape.length; i++) {
       for (let j = 0; j < piece.shape[i].length; j++) {
         if (piece.shape[i][j] && this.grid[piece.position.y + i] !== undefined) {
-          this.grid[piece.position.y + i][piece.position.x + j] = 0;
+          this.grid[piece.position.y + i][piece.position.x + j] = {
+            value: 0,
+            color: '',
+          };
         }
       }
     }
