ea21a25c15409a12d058b7b456d8be62d8da0c16
Davi Gabriel Scottini Adriano <davi.fadriano@gmail.com>
2025-01-09 21:53:04 -0300

    fix: Changing checkCollision to disregard the right and left sides

--- src/app/classes/GameRules.ts
+++ src/app/classes/GameRules.ts
@@ -1,68 +1,92 @@
-import { Game } from "./Game";
+import { Game } from './Game';
 
 export class GameRules {
   game: Game;
 
-  constructor(game: Game){
+  constructor(game: Game) {
     this.game = game;
   }
 
-  initializeBoard(){
+  initializeBoard() {
     this.game.initializePieces();
     this.game.pickNextPiece();
     this.game.addRandomPiece();
   }
 
-  checkCollision(newPosition: { x: number, y: number }): boolean {
+  checkCollision(newPosition: { x: number; y: number }): boolean {
     this.game.board.cleanPiece(this.game.currentPiece);
+
     for (let i = 0; i < this.game.currentPiece.shape.length; i++) {
       for (let j = 0; j < this.game.currentPiece.shape[i].length; j++) {
-        if (this.game.currentPiece.shape[i][j] && 
-            (newPosition.y + i >= this.game.board.height || 
-             newPosition.x + j < 0 || 
-             newPosition.x + j >= this.game.board.width || 
-             this.game.board.grid[newPosition.y + i]?.[newPosition.x + j] !== 0)) {
-              this.game.addPiece();
-          return true;
+        if (this.game.currentPiece.shape[i][j]) {
+          const y = newPosition.y + i;
+
+          if (
+            y >= this.game.board.height ||
+            (this.game.board.grid[y]?.[newPosition.x + j]?.value !== 0)
+          ) {
+            this.game.addPiece();
+            return true;
+          }
         }
       }
     }
+
     this.game.addPiece();
     return false;
   }
 
-  movePieceDown(){
-    const newPosition = { x: this.game.currentPiece.position.x, y: this.game.currentPiece.position.y + 1 };
-    this.game.board.cleanPiece(this.game.currentPiece);
-    this.game.currentPiece.position = newPosition;
-    this.game.addPiece();
+  movePieceDown() {
+    const newPosition = {
+      x: this.game.currentPiece.position.x,
+      y: this.game.currentPiece.position.y + 1,
+    };
+    if (this.checkCollision(newPosition)) {
+      this.game.loadNewPiece();
+    } else {
+      this.cleanAndSetNewPosition(newPosition);
+    }
+  }
+  movePieceLeft() {
+    const newPosition = {
+      x: this.game.currentPiece.position.x - 1,
+      y: this.game.currentPiece.position.y,
+    };
+    if (!this.checkCollision(newPosition)) {
+      this.cleanAndSetNewPosition(newPosition);
+    }
+  }
+  movePieceRight() {
+    const newPosition = {
+      x: this.game.currentPiece.position.x + 1,
+      y: this.game.currentPiece.position.y,
+    };
+    if (!this.checkCollision(newPosition)) {
+      this.cleanAndSetNewPosition(newPosition);
+    }
   }
 
-  movePieceLeft(){
-    const newPosition = { x: this.game.currentPiece.position.x + 1, y: this.game.currentPiece.position.y };
+  movePieceDownFast() {
+    let newPosition = {
+      x: this.game.currentPiece.position.x,
+      y: this.game.currentPiece.position.y,
+    };
+
+    while (!this.checkCollision({ x: newPosition.x, y: newPosition.y + 1 })) {
+      newPosition.y += 1;
+    }
+
+    this.cleanAndSetNewPosition(newPosition);
+    this.game.loadNewPiece();
+  }
+
+  rotatePiece() {}
+  leftRotatePiece() {}
+  rightRotatePiece() {}
+  holdPiece() {}
+  cleanAndSetNewPosition(newPosition: { x: number; y: number }) {
     this.game.board.cleanPiece(this.game.currentPiece);
     this.game.currentPiece.position = newPosition;
     this.game.addPiece();
-  }
-  movePieceRight(){
-    const newPosition = { x: this.game.currentPiece.position.x - 1, y: this.game.currentPiece.position.y };
-    this.game.board.cleanPiece(this.game.currentPiece);
-    this.game.currentPiece.position = newPosition;
-    this.game.addPiece();
-  }
-  movePieceDownFast(){
-    
-  }
-  rotatePiece(){
-    
-  }
-  leftRotatePiece(){
-    
-  }
-  rightRotatePiece(){
-    
-  }
-  holdPiece(){
-    
   }
 }
